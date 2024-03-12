475ac5e7f96577707b61793e9989f6e2e336373a
sayutef <233349@ids.upchiapas.edu.mx>
2024-03-11 18:04:50 -0600

    My commit4

--- src/main/java/com/sayuri/wifi/models/Producto.java
+++ src/main/java/com/sayuri/wifi/models/Producto.java
@@ -2,6 +2,13 @@ package com.sayuri.wifi.models;
 
 public class Producto {
     protected String nombre;
+    protected int costo;
+
+    public Producto(){}
+    public Producto(String nombre, int costo){
+        this.nombre = nombre;
+        this.costo = costo;
+    }
 
 
 
