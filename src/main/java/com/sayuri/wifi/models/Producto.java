371055b871e84a8fb500e3913cbcc80c1862d838
sayutef <233349@ids.upchiapas.edu.mx>
2024-03-11 18:06:20 -0600

    My commit6

--- src/main/java/com/sayuri/wifi/models/Producto.java
+++ src/main/java/com/sayuri/wifi/models/Producto.java
@@ -10,6 +10,27 @@ public class Producto {
         this.costo = costo;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", costo=" + costo +
+                '}';
+    }
 }
