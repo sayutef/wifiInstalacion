181227ac787f76b7786eb5eb46dc621b93b8955b
sayutef <233349@ids.upchiapas.edu.mx>
2024-03-16 14:28:23 -0600

    My commit13

--- src/main/java/com/sayuri/wifi/controllers/añadirPaqueteController.java
+++ src/main/java/com/sayuri/wifi/controllers/añadirPaqueteController.java
@@ -1,4 +1,47 @@
 package com.sayuri.wifi.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class añadirPaqueteController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField textNamePaquete;
+
+    @FXML
+    private TextField textCosto;
+
+    @FXML
+    private TextField textDescription;
+
+    @FXML
+    private Button bttonAgregar;
+
+    @FXML
+    private Button bttonReturn;
+
+    @FXML
+    void bttonAgregar(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonReturn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
 }
