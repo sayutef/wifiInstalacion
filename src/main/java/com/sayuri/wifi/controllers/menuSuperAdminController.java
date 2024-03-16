46f5a0965d28a5a28e18effa5175f41d340eadbc
Miltonvaz <233427@ids.upchiapas.edu.mx>
2024-03-16 14:34:25 -0600

    commit menu SuperAdmin

--- src/main/java/com/sayuri/wifi/controllers/menuSuperAdminController.java
+++ src/main/java/com/sayuri/wifi/controllers/menuSuperAdminController.java
@@ -1,4 +1,120 @@
 package com.sayuri.wifi.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class menuSuperAdminController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button bttonAddTec;
+
+    @FXML
+    private Button bttonDelateTec;
+
+    @FXML
+    private Button bttonDelatePaquete;
+
+    @FXML
+    private Button bttonSeePaquete;
+
+    @FXML
+    private Button bttonSeeTecnicos;
+
+    @FXML
+    private Button bttonAddPaquete;
+
+    @FXML
+    private Button bttonSeeUser;
+
+    @FXML
+    private Button bttonDelateUser;
+
+    @FXML
+    private Button bttonAddUser;
+
+    @FXML
+    private Button bttonDelateProductos;
+
+    @FXML
+    private Button bttonAddProductos;
+
+    @FXML
+    void bttonAddPaquete(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonAddProductos(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonAddTec(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonAddUser(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonDelatePaquete(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonDelateProductos(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonDelateTec(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonDelateUser(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonSeePaquete(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonSeeTecnicos(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonSeeUser(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert bttonAddTec != null : "fx:id=\"bttonAddTec\" was not injected: check your FXML file 'menuSuperAdmin.fxml'.";
+        assert bttonDelateTec != null : "fx:id=\"bttonDelateTec\" was not injected: check your FXML file 'menuSuperAdmin.fxml'.";
+        assert bttonDelatePaquete != null : "fx:id=\"bttonDelatePaquete\" was not injected: check your FXML file 'menuSuperAdmin.fxml'.";
+        assert bttonSeePaquete != null : "fx:id=\"bttonSeePaquete\" was not injected: check your FXML file 'menuSuperAdmin.fxml'.";
+        assert bttonSeeTecnicos != null : "fx:id=\"bttonSeeTecnicos\" was not injected: check your FXML file 'menuSuperAdmin.fxml'.";
+        assert bttonAddPaquete != null : "fx:id=\"bttonAddPaquete\" was not injected: check your FXML file 'menuSuperAdmin.fxml'.";
+        assert bttonSeeUser != null : "fx:id=\"bttonSeeUser\" was not injected: check your FXML file 'menuSuperAdmin.fxml'.";
+        assert bttonDelateUser != null : "fx:id=\"bttonDelateUser\" was not injected: check your FXML file 'menuSuperAdmin.fxml'.";
+        assert bttonAddUser != null : "fx:id=\"bttonAddUser\" was not injected: check your FXML file 'menuSuperAdmin.fxml'.";
+        assert bttonDelateProductos != null : "fx:id=\"bttonDelateProductos\" was not injected: check your FXML file 'menuSuperAdmin.fxml'.";
+        assert bttonAddProductos != null : "fx:id=\"bttonAddProductos\" was not injected: check your FXML file 'menuSuperAdmin.fxml'.";
+
+    }
 }
