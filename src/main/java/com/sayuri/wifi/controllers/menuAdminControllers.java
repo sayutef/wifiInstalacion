6f7d730da006cd1e86e97f98b7d61c99141ef527
Miltonvaz <233427@ids.upchiapas.edu.mx>
2024-03-16 16:10:45 -0600

    commit menu admin

--- src/main/java/com/sayuri/wifi/controllers/menuAdminControllers.java
+++ src/main/java/com/sayuri/wifi/controllers/menuAdminControllers.java
@@ -1,4 +1,106 @@
 package com.sayuri.wifi.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class menuAdminControllers {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button bttonSeeTecnicos;
+
+    @FXML
+    private Button bttonAddProducto;
+
+    @FXML
+    private Button bttonDelateProducto;
+
+    @FXML
+    private Button bttonImprimir;
+
+    @FXML
+    private Button bttonComprobante;
+
+    @FXML
+    private Button bttonSeeUser;
+
+    @FXML
+    private Button bttonSeeOrdenes;
+
+    @FXML
+    private Button bttonGenerarReporte;
+
+    @FXML
+    private Button bttonDelateReporte;
+
+    @FXML
+    private Button bttonSalir;
+
+    @FXML
+    void bttonComprobante(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonDelateProducto(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonDelateReporte(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonGenerarReporte(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonImprimir(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonSalir(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonSeeOrdenes(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonSeeTecnicos(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonSeeUser(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert bttonSeeTecnicos != null : "fx:id=\"bttonSeeTecnicos\" was not injected: check your FXML file 'menuAdmi.fxml'.";
+        assert bttonAddProducto != null : "fx:id=\"bttonAddProducto\" was not injected: check your FXML file 'menuAdmi.fxml'.";
+        assert bttonDelateProducto != null : "fx:id=\"bttonDelateProducto\" was not injected: check your FXML file 'menuAdmi.fxml'.";
+        assert bttonImprimir != null : "fx:id=\"bttonImprimir\" was not injected: check your FXML file 'menuAdmi.fxml'.";
+        assert bttonComprobante != null : "fx:id=\"bttonComprobante\" was not injected: check your FXML file 'menuAdmi.fxml'.";
+        assert bttonSeeUser != null : "fx:id=\"bttonSeeUser\" was not injected: check your FXML file 'menuAdmi.fxml'.";
+        assert bttonSeeOrdenes != null : "fx:id=\"bttonSeeOrdenes\" was not injected: check your FXML file 'menuAdmi.fxml'.";
+        assert bttonGenerarReporte != null : "fx:id=\"bttonGenerarReporte\" was not injected: check your FXML file 'menuAdmi.fxml'.";
+        assert bttonDelateReporte != null : "fx:id=\"bttonDelateReporte\" was not injected: check your FXML file 'menuAdmi.fxml'.";
+        assert bttonSalir != null : "fx:id=\"bttonSalir\" was not injected: check your FXML file 'menuAdmi.fxml'.";
+
+    }
 }
