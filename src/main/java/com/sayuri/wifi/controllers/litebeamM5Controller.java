09f33e01b1b5751ff90be4331e4eb2864fea4e51
sayutef <233349@ids.upchiapas.edu.mx>
2024-03-16 15:23:09 -0600

    My commit16

--- src/main/java/com/sayuri/wifi/controllers/litebeamM5Controller.java
+++ src/main/java/com/sayuri/wifi/controllers/litebeamM5Controller.java
@@ -1,4 +1,50 @@
 package com.sayuri.wifi.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class litebeamM5Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField textName;
+
+    @FXML
+    private TextField textCosto;
+
+    @FXML
+    private TextField textTipoConexion;
+
+    @FXML
+    private TextField textPotencia;
+
+    @FXML
+    private Button bttonAdd;
+
+    @FXML
+    private Button bttonReturn;
+
+    @FXML
+    void bttonAdd(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonReturn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
 }
--- src/main/java/com/sayuri/wifi/controllers/mastilController.java
+++ src/main/java/com/sayuri/wifi/controllers/mastilController.java
@@ -1,4 +1,50 @@
 package com.sayuri.wifi.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class mastilController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField textMastil;
+
+    @FXML
+    private TextField textCostoMastil;
+
+    @FXML
+    private TextField textMaterialMastil;
+
+    @FXML
+    private TextField textAlturaMastil;
+
+    @FXML
+    private Button bttonAdd;
+
+    @FXML
+    private Button bttonReturn;
+
+    @FXML
+    void bttonAdd(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonReturn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
 }
