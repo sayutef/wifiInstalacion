ec6dfa476ceab2399e9b538e3eec5314b2bc745d
sayutef <233349@ids.upchiapas.edu.mx>
2024-03-16 15:31:04 -0600

    My commit17

--- src/main/java/com/sayuri/wifi/models/LitebeamM5.java
+++ src/main/java/com/sayuri/wifi/models/LitebeamM5.java
@@ -3,4 +3,38 @@ package com.sayuri.wifi.models;
 public class LitebeamM5 extends Producto{
     private String tipoConexion;
     private String potenciaTransmicion;
+
+    public LitebeamM5(){super();}
+
+    public LitebeamM5(String nombre, int costo, String tipoConexion, String potenciaTransmicion) {
+        super(nombre, costo);
+        this.tipoConexion = tipoConexion;
+        this.potenciaTransmicion = potenciaTransmicion;
+    }
+
+    @Override
+    public String toString() {
+        return "LitebeamM5{" +
+                "tipoConexion='" + tipoConexion + '\'' +
+                ", potenciaTransmicion='" + potenciaTransmicion + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", costo=" + costo +
+                '}';
+    }
+
+    public String getTipoConexion() {
+        return tipoConexion;
+    }
+
+    public void setTipoConexion(String tipoConexion) {
+        this.tipoConexion = tipoConexion;
+    }
+
+    public String getPotenciaTransmicion() {
+        return potenciaTransmicion;
+    }
+
+    public void setPotenciaTransmicion(String potenciaTransmicion) {
+        this.potenciaTransmicion = potenciaTransmicion;
+    }
 }
--- src/main/java/com/sayuri/wifi/models/Mastil.java
+++ src/main/java/com/sayuri/wifi/models/Mastil.java
@@ -3,4 +3,40 @@ package com.sayuri.wifi.models;
 public class Mastil extends Producto{
     private String material;
     private int altura;
+
+    public Mastil() {
+        super();
+    }
+
+    public Mastil(String nombre, int costo, String material, int altura) {
+        super(nombre, costo);
+        this.material = material;
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Mastil{" +
+                "material='" + material + '\'' +
+                ", altura=" + altura +
+                ", nombre='" + nombre + '\'' +
+                ", costo=" + costo +
+                '}';
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
 }
