5ab03e980ddfa77bfe22471171df8573b202e167
sayutef <233349@ids.upchiapas.edu.mx>
2024-03-17 22:32:59 -0600

    My commit21

--- src/main/java/com/sayuri/wifi/controllers/viewEmpleadosController.java
+++ src/main/java/com/sayuri/wifi/controllers/viewEmpleadosController.java
@@ -1,4 +1,49 @@
 package com.sayuri.wifi.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.sayuri.wifi.HelloApplication;
+import com.sayuri.wifi.models.Administrador;
+import com.sayuri.wifi.models.Persona;
+import com.sayuri.wifi.models.Tecnico;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
 public class viewEmpleadosController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ListView<String> listViewEmpleado;
+
+    @FXML
+    private Button bttonReturn;
+
+    @FXML
+    private Button bttonSee;
+
+    @FXML
+    void bttonReturn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void bttonSee(MouseEvent event) {
+        Administrador tecnicos = HelloApplication.getAdmin();
+        for (Persona imprimir : tecnicos.getListPersona()){
+            listViewEmpleado.getItems().add(imprimir.toString());
+        }
+    }
+
+    @FXML
+    void initialize() {
+
+    }
 }
---
+++ src/main/java/com/sayuri/wifi/models/Persona.java
@@ -0,0 +1,35 @@
+package com.sayuri.wifi.models;
+
+public class Persona {
+    protected String nombre;
+    protected String apellido;
+    public Persona(){}
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                '}';
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
