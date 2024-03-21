123ba478acdca224c8552af04d87cecc7c27aaf6
Miltonvaz <233427@ids.upchiapas.edu.mx>
2024-03-21 03:00:33 -0600

    commit Añadir Usuario

--- src/main/java/com/sayuri/wifi/controllers/añadirUsuarioController.java
+++ src/main/java/com/sayuri/wifi/controllers/añadirUsuarioController.java
@@ -2,7 +2,12 @@ package com.sayuri.wifi.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import com.sayuri.wifi.HelloApplication;
+import com.sayuri.wifi.models.Tecnico;
+import com.sayuri.wifi.models.Usuario;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -19,7 +24,7 @@ public class añadirUsuarioController {
     private TextField textNameUser;
 
     @FXML
-    private TextField textApellido;
+    private TextField textApellidoUser;
 
     @FXML
     private TextField textDireccion;
@@ -28,25 +33,45 @@ public class añadirUsuarioController {
     private Button bttonAdd;
 
     @FXML
-    private Button bttonReturn;
+    private Button bttonVolver;
 
     @FXML
     void bttonAdd(MouseEvent event) {
+        try {
+            String nombreUser = textNameUser.getText();
+            String apellidoUser = textApellidoUser.getText();
+            String direccion  = textDireccion.getText();
+            if (nombreUser.isEmpty() || apellidoUser.isEmpty()) {
+                throw new IllegalArgumentException("Faltan datos por introducir en el formulario.");
+            }
+           Usuario user = new Usuario(nombreUser, apellidoUser, direccion);
+            HelloApplication.getAdmin().addPersona(user);
 
+            System.out.println("Nombre : [" + user.getNombre() + "]");
+            System.out.println("Apellido : [" + user.getApellido() + "]");
+            System.out.println("Dirección : [" + user.getDireccion() + "]");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Datos introducidos en el formulario...");
+            alert.setHeaderText("Ha introducido correctamente los " +
+                    "datos en el formulario. Se muestran a continuación.");
+            alert.setContentText(user.toString());
+
+            alert.showAndWait();
+        }catch (IllegalArgumentException e){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Faltan datos...");
+            alert.setHeaderText(e.getMessage());
+            alert.showAndWait();
+        }
     }
 
     @FXML
-    void bttonReturn(MouseEvent event) {
+    void bttonVolver(MouseEvent event) {
 
     }
 
     @FXML
     void initialize() {
-        assert textNameUser != null : "fx:id=\"textNameUser\" was not injected: check your FXML file 'agregarUsuario.fxml'.";
-        assert textApellido != null : "fx:id=\"textApellido\" was not injected: check your FXML file 'agregarUsuario.fxml'.";
-        assert textDireccion != null : "fx:id=\"textDireccion\" was not injected: check your FXML file 'agregarUsuario.fxml'.";
-        assert bttonAdd != null : "fx:id=\"bttonAdd\" was not injected: check your FXML file 'agregarUsuario.fxml'.";
-        assert bttonReturn != null : "fx:id=\"bttonReturn\" was not injected: check your FXML file 'agregarUsuario.fxml'.";
 
     }
 }
